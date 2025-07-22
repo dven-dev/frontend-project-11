feb6b6f8e7c8a98230b1cff49ddacba2f7014c86
dven-dev <me@dven.dev>
2025-07-22 12:52:06 +0300

    fix eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@stylistic/eslint-plugin-js';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      js,
+    },
+    rules: {
+      ...js.configs['flat/recommended']?.rules,
+      'no-console': 'off',
+    },
+  },
+];
